78829670e39f2eca6c5815d55ff85661f436102f
pedrinabrasilifrn <pedrina.brasil@gmail.com>
2026-08-21 14:12:46 -0300

    criacao de variaveis

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var nasc = 1991;
+let nome = "Pedrina";
+const viva = true;
+
+function calcIdade(ano=2026){
+    let idade = ano - nasc;
+    alert(`Dentro de Função - Idade ${idade}`);
+    return idade;
+}
+
+calcIdade();
+/*
+alert(`Fora da funcao : Idade ${idade}`); 
+Erro pois a variavel let nao existe fora do escopo dela
+*/
+alert(`Fora da funcao : chamando calcIdade ${calcIdade(2027)}`);
