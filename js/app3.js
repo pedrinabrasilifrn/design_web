5097c69d2ac89a7dfb9a418e3bd07d0eb073c8d3
pedrinabrasilifrn <pedrina.brasil@gmail.com>
2026-08-28 13:34:55 -0300

    fim aula4

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 1991;
+let nome = "Pedrina";
+let viva = true;
+let altura = 1.64;
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`Ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`Nome - tipo: ${typeof(nome)}`)
+alert(`Viva - tipo: ${typeof(viva)}`)
+alert(`Altura - tipo: ${typeof(altura)}`)
+alert(`Sexo - tipo: ${typeof(sexo)}`)
+alert(`Peso - tipo: ${typeof(peso)}`)
+alert(`IMC - tipo: ${typeof(imc)}`)
