3aace8bf18faef4104e7e7229c8396e33967445f
pedrinabrasilifrn <pedrina.brasil@gmail.com>
2026-09-04 14:27:00 -0300

    break e continue

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,12 @@
 let vezes = Number(prompt("Digite o numero de vezes:"));
 for (let i = 1; i<=vezes; i++){
+    if (vezes > 100){
+        alert("Valor inválido, recarregue a página e digite um valor de 0 a 100");
+        break
+    }
     alert(`Contei ${i} vez`);
+    if (i%2!=0){ //é impar?
+        continue; //se impar, continua = voltar para o inicio
+    }
+    alert(`${i} é par`);    
 }
