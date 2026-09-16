fdeac8f6c7cb2c31db079e6757224bc4613e1729
pedrinabrasilifrn <pedrina.brasil@gmail.com>
2026-09-16 17:55:34 -0300

    ultimos eventos

--- js/app1.js
+++ js/app1.js
@@ -32,3 +32,53 @@ function selecionar(e){
 const cbbox = document.getElementById("combobox");
 cbbox.addEventListener("change", selecionar);
 
+const opacidade = document.getElementById("opacidade");
+opacidade.addEventListener("input", function(e){
+    e.preventDefault();
+    let valor = opacidade.value;
+    imagem.setAttribute("style", `opacity:${valor}`);
+});
+
+const li1 = document.getElementById("li1");
+li1.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 1;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+
+const li2 = document.getElementById("li2");
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 2;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+
+const li3 = document.getElementById("li3");
+li3.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 3;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+
+const li4 = document.getElementById("li4");
+li4.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 4;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+
+
+const li5 = document.getElementById("li5");
+li5.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 5;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+
+
+const li6 = document.getElementById("li6");
+li6.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 6;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
