9946cf540e759e8eb8cd26241901e5e6bd29a860
pedrinabrasilifrn <pedrina.brasil@gmail.com>
2026-09-18 14:28:43 -0300

    arrays

---
+++ js/app1.js
@@ -0,0 +1,7 @@
+const amigos = ["Pedro", "Ana", "Clara"];
+tam = amigos.length
+
+console.log(`Arranjo ${amigos}`);
+console.log(`Tamanho ${tam}`);
+console.log(`Primeiro ${amigos[0]}`);
+console.log(`Último ${amigos[tam-1]}`);
